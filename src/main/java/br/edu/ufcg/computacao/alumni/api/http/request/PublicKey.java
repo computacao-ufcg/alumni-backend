33af0e10df6495310b1263680c42eddadf800c53
natalia-sa <natalia.andre@ccc.ufcg.edu.br>
2020-10-05 11:48:25 -0300

    creating class PublicKey on ApiDocumentation

--- src/main/java/br/edu/ufcg/computacao/alumni/api/http/request/PublicKey.java
+++ src/main/java/br/edu/ufcg/computacao/alumni/api/http/request/PublicKey.java
@@ -1,9 +1,12 @@
 package br.edu.ufcg.computacao.alumni.api.http.request;
 
+import br.edu.ufcg.computacao.alumni.constants.ApiDocumentation;
 import br.edu.ufcg.computacao.alumni.constants.Messages;
 import br.edu.ufcg.computacao.alumni.constants.SystemConstants;
 import br.edu.ufcg.computacao.alumni.core.ApplicationFacade;
 import br.edu.ufcg.computacao.alumni.api.http.response.PublicKeyResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +15,14 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RestController
 @RequestMapping(value = PublicKey.PUBLIC_KEY_ENDPOINT)
-//@Api(description = ApiDocumentation.PublicKey.API)
+@Api(description = ApiDocumentation.PublicKey.API)
 
 public class PublicKey {
     public static final String PUBLIC_KEY_ENDPOINT = SystemConstants.SERVICE_BASE_ENDPOINT + "publicKey";
 
     private final Logger LOGGER = Logger.getLogger(PublicKey.class);
 
-//    @ApiOperation(value = ApiDocumentation.PublicKey.GET_OPERATION)
+    @ApiOperation(value = ApiDocumentation.PublicKey.GET_OPERATION)
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<PublicKeyResponse> getPublicKey() throws Exception {
 
