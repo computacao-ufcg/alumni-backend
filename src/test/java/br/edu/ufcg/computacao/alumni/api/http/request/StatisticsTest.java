f1ab1a3ac70b2043119723b4e0659d5d9dd8ca46
Natália Salvino <natalia.andre@ccc.ufcg.edu.br>
2020-12-02 10:35:44 -0300

    creating StatisticsTest

---
+++ src/test/java/br/edu/ufcg/computacao/alumni/api/http/request/StatisticsTest.java
@@ -0,0 +1,78 @@
+package br.edu.ufcg.computacao.alumni.api.http.request;
+
+import br.edu.ufcg.computacao.alumni.api.http.CommonKeys;
+import br.edu.ufcg.computacao.alumni.api.http.response.StatisticsResponse;
+import br.edu.ufcg.computacao.alumni.core.ApplicationFacade;
+import br.edu.ufcg.computacao.alumni.core.models.StatisticsModel;
+import br.edu.ufcg.computacao.eureca.common.exceptions.EurecaException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(SpringRunner.class)
+@WebMvcTest(value = Statistics.class, secure = false)
+@PrepareForTest(ApplicationFacade.class)
+public class StatisticsTest {
+
+    private static final String STATISTICS_ENDPOINT = Statistics.ENDPOINT;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ApplicationFacade facade;
+
+    @Before
+    public void setUp() {
+        this.facade = Mockito.spy(ApplicationFacade.class);
+        PowerMockito.mockStatic(ApplicationFacade.class);
+        BDDMockito.given(ApplicationFacade.getInstance()).willReturn(this.facade);
+    }
+
+    @Test
+    public void getStatisticsTest() throws Exception {
+        Mockito.doReturn(getFakeStatistics()).when(this.facade).getStatistics(Mockito.anyString(),Mockito.anyObject(), Mockito.anyObject());
+        HttpHeaders headers = getHttpHeaders();
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(STATISTICS_ENDPOINT + "/?level=undergraduate&courseName=computing-science")
+                .headers(headers).contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        int expectedStatus = HttpStatus.OK.value();
+
+        Assert.assertEquals(expectedStatus, result.getResponse().getStatus());
+        Mockito.verify(this.facade, Mockito.times(1))
+                .getStatistics(Mockito.anyString(), Mockito.anyObject(), Mockito.anyObject());
+    }
+
+    private HttpHeaders getHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        String fakeUserToken = "fake-access-id";
+        headers.set(CommonKeys.AUTHENTICATION_TOKEN_KEY, fakeUserToken);
+        return headers;
+    }
+
+    private StatisticsResponse getFakeStatistics() {
+        StatisticsModel modelCourse =  new StatisticsModel(0,0,0
+                ,0, 0, 0, 0);
+        StatisticsModel modelLevel = new StatisticsModel(0, 0
+                , 0, 0, 0, 0, 0);
+        StatisticsResponse statistics = new StatisticsResponse(modelCourse, modelLevel);
+        return statistics;
+    }
+}
