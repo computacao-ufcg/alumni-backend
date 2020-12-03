c6ef75cbb45245da0657e30ad57e67b0ef3814b3
Natália Salvino <natalia.andre@ccc.ufcg.edu.br>
2020-12-02 23:29:08 -0300

    adding tests in AlumnusTest

--- src/test/java/br/edu/ufcg/computacao/alumni/api/http/request/AlumnusTest.java
+++ src/test/java/br/edu/ufcg/computacao/alumni/api/http/request/AlumnusTest.java
@@ -1,12 +1,9 @@
 package br.edu.ufcg.computacao.alumni.api.http.request;
 
 import br.edu.ufcg.computacao.alumni.api.http.CommonKeys;
-import br.edu.ufcg.computacao.alumni.api.http.response.UfcgAlumnusData;
-import br.edu.ufcg.computacao.alumni.constants.ConfigurationPropertyDefaults;
-import br.edu.ufcg.computacao.alumni.constants.SystemConstants;
 import br.edu.ufcg.computacao.alumni.core.ApplicationFacade;
-import br.edu.ufcg.computacao.alumni.core.holders.AlumniHolder;
-import br.edu.ufcg.computacao.eureca.common.exceptions.EurecaException;
+import br.edu.ufcg.computacao.eureca.common.exceptions.UnauthenticatedUserException;
+import br.edu.ufcg.computacao.eureca.common.exceptions.UnauthorizedRequestException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,24 +15,17 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
@@ -48,14 +38,11 @@ import java.util.List;
 public class AlumnusTest {
 
     private static final String ALUMNUS_ENDPOINT = Alumnus.ENDPOINT;
-    private static final String ALUMNI_NAMES_SUFIX = Alumnus.ENDPOINT + "/names";
-    private static final String ALUMNI_CURRENT_JOB_SUFIX = Alumnus.ENDPOINT + "/currentJob";
 
     @Autowired
     private MockMvc mockMvc;
 
     private ApplicationFacade facade;
-
 
     @Before
     public void setUp() {
@@ -66,47 +53,170 @@ public class AlumnusTest {
 
     @Test
     public void getAlumniTest() throws Exception {
-        Mockito.doReturn(getFakePage()).when(this.facade).getAlumniData(Mockito.anyString(),Mockito.anyInt());
+        String getAlumniEndpoint = ALUMNUS_ENDPOINT + "/0";
+
+        Mockito.doReturn(createFakePage()).when(this.facade).getAlumniData(Mockito.anyString(),Mockito.anyInt());
+
         HttpHeaders headers = getHttpHeaders();
 
-        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(ALUMNUS_ENDPOINT + "/0")
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(getAlumniEndpoint)
                 .headers(headers).contentType(MediaType.APPLICATION_JSON)).andReturn();
         int expectedStatus = HttpStatus.OK.value();
 
         Assert.assertEquals(expectedStatus, result.getResponse().getStatus());
-        System.out.println(result.getResponse().getContentAsString());
-        Mockito.verify(this.facade, Mockito.times(1)).getAlumniData(Mockito.anyString(),Mockito.anyInt());
+
+        Mockito.verify(this.facade, Mockito.times(1))
+                .getAlumniData(Mockito.anyString(),Mockito.anyInt());
     }
 
     @Test
-    public void getAlumniNames() throws Exception {
-        Mockito.doReturn(getFakePage()).when(this.facade).getAlumniNames(Mockito.anyString(),Mockito.anyInt());
+    public void getAlumniNamesTest() throws Exception {
+        String getAlumniNamesEndpoint = ALUMNUS_ENDPOINT + "/names/0";
+
+        Mockito.doReturn(createFakePage()).when(this.facade).getAlumniNames(Mockito.anyString(),Mockito.anyInt());
         HttpHeaders headers = getHttpHeaders();
 
-        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(ALUMNI_NAMES_SUFIX + "/0")
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(getAlumniNamesEndpoint)
                 .headers(headers).contentType(MediaType.APPLICATION_JSON)).andReturn();
 
         int expectedStatus = HttpStatus.OK.value();
 
         Assert.assertEquals(expectedStatus, result.getResponse().getStatus());
-        System.out.println(result.getResponse().getContentAsString());
-        Mockito.verify(this.facade, Mockito.times(1)).getAlumniNames(Mockito.anyString(),Mockito.anyInt());
+
+        Mockito.verify(this.facade, Mockito.times(1))
+                .getAlumniNames(Mockito.anyString(),Mockito.anyInt());
     }
 
     @Test
-    public void getAlumnusCurrentJob() throws Exception {
-        Mockito.doReturn(getFakePage()).when(this.facade).getAlumniCurrentJob(Mockito.anyString(),Mockito.anyInt());
+    public void getAlumniCurrentJobTest() throws Exception {
+        String getAlumniCurrentJobEndpoint = ALUMNUS_ENDPOINT + "/currentJob/0";
+
+        Mockito.doReturn(createFakePage()).when(this.facade).getAlumniCurrentJob(Mockito.anyString(),Mockito.anyInt());
         HttpHeaders headers = getHttpHeaders();
 
-        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(ALUMNI_CURRENT_JOB_SUFIX + "/0")
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(getAlumniCurrentJobEndpoint)
                 .headers(headers).contentType(MediaType.APPLICATION_JSON)).andReturn();
 
         int expectedStatus = HttpStatus.OK.value();
 
         Assert.assertEquals(expectedStatus, result.getResponse().getStatus());
-        System.out.println(result.getResponse().getContentAsString());
-        Mockito.verify(this.facade, Mockito.times(1)).getAlumniCurrentJob(Mockito.anyString(),Mockito.anyInt());
 
+        Mockito.verify(this.facade, Mockito.times(1))
+                .getAlumniCurrentJob(Mockito.anyString(),Mockito.anyInt());
+
+    }
+
+    @Test
+    public void getAlumniUnauthorizedExceptionTest() throws Exception {
+        String getAlumniEndpoint = ALUMNUS_ENDPOINT + "/0";
+
+        Mockito.doThrow(new UnauthorizedRequestException()).when(this.facade)
+                .getAlumniData(Mockito.anyString(), Mockito.anyInt());
+
+        HttpHeaders headers = getHttpHeaders();
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(getAlumniEndpoint)
+                .headers(headers).contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        int expectedStatus = HttpStatus.FORBIDDEN.value();
+
+        Assert.assertEquals(expectedStatus, result.getResponse().getStatus());
+        Mockito.verify(this.facade, Mockito.times(1))
+                .getAlumniData(Mockito.anyString(), Mockito.anyInt());
+    }
+
+    @Test
+    public void getAlumniNamesUnauthorizedExceptionTest() throws Exception {
+        String getAlumniNamesEndpoint = ALUMNUS_ENDPOINT + "/names/0";
+        Mockito.doThrow(new UnauthorizedRequestException()).when(this.facade)
+                .getAlumniNames(Mockito.anyString(), Mockito.anyInt());
+
+        HttpHeaders headers = getHttpHeaders();
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(getAlumniNamesEndpoint)
+                .headers(headers).contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        int expectedStatus = HttpStatus.FORBIDDEN.value();
+
+        Assert.assertEquals(expectedStatus, result.getResponse().getStatus());
+        Mockito.verify(this.facade, Mockito.times(1))
+                .getAlumniNames(Mockito.anyString(), Mockito.anyInt());
+    }
+
+    @Test
+    public void getAlumniCurrentJobUnauthorizedExceptionTest() throws Exception {
+        String getAlumniCurrentJobEndpoint = ALUMNUS_ENDPOINT + "/currentJob/0";
+
+        Mockito.doThrow(new UnauthorizedRequestException()).when(this.facade)
+                .getAlumniCurrentJob(Mockito.anyString(), Mockito.anyInt());
+
+        HttpHeaders headers = getHttpHeaders();
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(getAlumniCurrentJobEndpoint)
+                .headers(headers).contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        int expectedStatus = HttpStatus.FORBIDDEN.value();
+
+        Assert.assertEquals(expectedStatus, result.getResponse().getStatus());
+        Mockito.verify(this.facade, Mockito.times(1))
+                .getAlumniCurrentJob(Mockito.anyString(), Mockito.anyInt());
+    }
+
+    @Test
+    public void getAlumniUnauthenticatedExceptionTest() throws Exception {
+        String getAlumniEndpoint = ALUMNUS_ENDPOINT + "/0";
+
+        Mockito.doThrow(new UnauthenticatedUserException()).when(this.facade)
+                .getAlumniData(Mockito.anyString(), Mockito.anyInt());
+
+        HttpHeaders headers = getHttpHeaders();
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(getAlumniEndpoint)
+                .headers(headers).contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        int expectedStatus = HttpStatus.UNAUTHORIZED.value();
+        Assert.assertEquals(expectedStatus, result.getResponse().getStatus());
+
+        Mockito.verify(this.facade, Mockito.times(1))
+                .getAlumniData(Mockito.anyString(), Mockito.anyInt());
+    }
+
+    @Test
+    public void getAlumniNamesUnauthenticatedExceptionTest() throws Exception {
+        String getAlumniNamesEndpoint = ALUMNUS_ENDPOINT + "/names/0";
+
+        Mockito.doThrow(new UnauthenticatedUserException()).when(this.facade)
+                .getAlumniNames(Mockito.anyString(), Mockito.anyInt());
+
+        HttpHeaders headers = getHttpHeaders();
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(getAlumniNamesEndpoint)
+                .headers(headers).contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        int expectedStatus = HttpStatus.UNAUTHORIZED.value();
+        Assert.assertEquals(expectedStatus, result.getResponse().getStatus());
+
+        Mockito.verify(this.facade, Mockito.times(1))
+                .getAlumniNames(Mockito.anyString(), Mockito.anyInt());
+    }
+
+    @Test
+    public void getAlumniCurrentjobUnauthenticatedExceptionTest() throws Exception {
+        String getAlumniCurrentJobEndpoint = ALUMNUS_ENDPOINT + "/currentJob/0";
+
+        Mockito.doThrow(new UnauthenticatedUserException()).when(this.facade)
+                .getAlumniCurrentJob(Mockito.anyString(), Mockito.anyInt());
+
+        HttpHeaders headers = getHttpHeaders();
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(getAlumniCurrentJobEndpoint)
+                .headers(headers).contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        int expectedStatus = HttpStatus.UNAUTHORIZED.value();
+        Assert.assertEquals(expectedStatus, result.getResponse().getStatus());
+
+        Mockito.verify(this.facade, Mockito.times(1))
+                .getAlumniCurrentJob(Mockito.anyString(), Mockito.anyInt());
     }
 
     private HttpHeaders getHttpHeaders() {
@@ -116,16 +226,14 @@ public class AlumnusTest {
         return headers;
     }
 
-    private Page getFakePage() {
+    private Page createFakePage() {
         Pageable pageable= new PageRequest(0, 10);
 
         List list = new ArrayList<>();
         int start = (int) pageable.getOffset();
         int end = (int) ((start + pageable.getPageSize()) > list.size() ?
                 list.size() : (start + pageable.getPageSize()));
-
         Page page = new PageImpl<>(list.subList(start, end), pageable, list.size());
         return page;
-
     }
 }
