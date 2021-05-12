430b749bc4ae0ae794ef828df5a1c56cadd5d913
fubica <fubica@dsc.ufcg.edu.br>
2021-05-12 12:41:35 -0300

    Add debugging

--- src/main/java/br/edu/ufcg/computacao/alumni/constants/SystemConstants.java
+++ src/main/java/br/edu/ufcg/computacao/alumni/constants/SystemConstants.java
@@ -1,7 +1,7 @@
 package br.edu.ufcg.computacao.alumni.constants;
 
 public class SystemConstants {
-    public static final String SERVICE_BASE_ENDPOINT = "/";
+    public static final String SERVICE_BASE_ENDPOINT = "alumni/";
     public static final String API_VERSION_NUMBER = "1.0.0";
     public static final String CONFIG_FILE = "alumni.conf";
 }
