042043fc6e1ea1e61db6137325fc394f7acb5e61
fubica <fubica@dsc.ufcg.edu.br>
2021-08-22 11:40:53 -0300

    Added query field in HTTP request

--- src/main/java/br/edu/ufcg/computacao/alumni/constants/SystemConstants.java
+++ src/main/java/br/edu/ufcg/computacao/alumni/constants/SystemConstants.java
@@ -4,4 +4,6 @@ public class SystemConstants {
     public static final String SERVICE_BASE_ENDPOINT = "alumni/";
     public static final String API_VERSION_NUMBER = "1.0.0";
     public static final String CONFIG_FILE = "alumni.conf";
+    public static final String ALUMNI_QUERY = "courseCode=14102100";
+    public static final String EMPTY_QUERY = "";
 }
