719ad3576d6fcecb4b03b8b91e0329de608ad53c
fubica <fubica@dsc.ufcg.edu.br>
2022-03-08 10:03:44 -0300

    Fixed compilation error

--- src/main/java/br/edu/ufcg/computacao/alumni/api/http/AlumniExceptionToHttpErrorConditionTranslator.java
+++ src/main/java/br/edu/ufcg/computacao/alumni/api/http/AlumniExceptionToHttpErrorConditionTranslator.java
@@ -1,8 +1,8 @@
 package br.edu.ufcg.computacao.alumni.api.http;
 
-import br.edu.ufcg.computacao.eureca.common.exceptions.ExceptionToHttpErrorConditionTranslator;
+import br.edu.ufcg.computacao.eureca.common.exceptions.CommonExceptionHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
-public class AlumniExceptionToHttpErrorConditionTranslator extends ExceptionToHttpErrorConditionTranslator {
+public class AlumniExceptionToHttpErrorConditionTranslator extends CommonExceptionHandler {
 }
